b26c4cfd69dc5791d5b95e9f623e8256d2b43f41
Tim Nilsson <60481888+BKKinvader@users.noreply.github.com>
2023-04-28 18:37:41 +0200

    Update with consoleLock and Position

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Labb_3_Threads
+﻿using System.Diagnostics;
+
+namespace Labb_3_Threads
 {
     internal class Program
     {
@@ -9,10 +11,11 @@
 
         public static void StartRace()
         {
-            // Skapa threads för bilar
-            Thread car1Thread = new Thread(() => RaceCar("Mercedes", Thread.CurrentThread));
-            Thread car2Thread = new Thread(() => RaceCar("BMW", Thread.CurrentThread));
-            Thread car3Thread = new Thread(() => RaceCar("Lambo", Thread.CurrentThread));
+            // Skapa Bilar med Namn index och thread
+            Thread car1Thread = new Thread(() => RaceCar("Mercedes", 0, Thread.CurrentThread));
+            Thread car2Thread = new Thread(() => RaceCar("BMW", 1, Thread.CurrentThread));
+            Thread car3Thread = new Thread(() => RaceCar("Lambo", 2, Thread.CurrentThread));
+
 
             // Starta race
             car1Thread.Start();
@@ -23,35 +26,43 @@
             car1Thread.Join();
             car2Thread.Join();
             car3Thread.Join();
-
+            Console.Clear();
             Console.WriteLine($"Congrats {winner} Win!");
+            Console.ReadKey();
 
         }
 
-        //Sätta normalspeed och vinnaren
+        
         //Statisk variabel
         public static int speed = 120;
         public static string winner = "";
+        public static int[] carPositions = new int[] { 0, 2, 4 }; // Bilens position i consolen och consoleLock
+        public static object consoleLock = new object();
 
 
-
-        
-        //Tar in bilens namn och vilken thread
-        public static void RaceCar(string carName, Thread thread)
+        public static void RaceCar(string carName, int carIndex, Thread thread)
         {
+            double distance = 0; // distance in km
+            string accidentMessage = "";
             int carSpeed = speed;
             Console.WriteLine($"{carName} started");
+            Thread.Sleep(1000);
+            Console.Clear();
 
-            //Timer
+
+            //Timer för problem
             DateTime startTime = DateTime.Now;
             DateTime nextCheckTime = startTime.AddSeconds(10);
             bool timer30Seconds = false;
 
-            //10 km race
-            for (int i = 0; i <= 10; i++)
-            {
 
-                Console.WriteLine($"{carName}: {i} km");
+           
+
+
+            //10 km = 10 loop
+            while (distance <= 10)
+            {
+                UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
 
                 if (DateTime.Now >= nextCheckTime)
                 {
@@ -59,80 +70,83 @@
                     timer30Seconds = true;
                 }
 
-                //Besiktning
+                //Problem intröffar
                 if (timer30Seconds)
                 {
-                    Console.Clear();
-                    Console.WriteLine($"Car Check 10 sec {carName}");
+                   
                     //Kolla om bilen har punka
                     if (HasPuncture())
                     {
-                        Console.WriteLine($"!!! {carName} got a puncture! !!!");
+
+
+                        
+                        accidentMessage = $" :{carName} got a puncture!";
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
                         //Stoppa bilen 20 sekunder
                         Thread.Sleep(20000);
-                        Console.WriteLine($"<<< {carName} is back on track >>>>");
+                        accidentMessage = $" : {carName} is back on track";
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
+                        
                     }
 
 
                     if (OutOfGas())
                     {
-                        Console.WriteLine($"!!! {carName} ran out of gas! !!!");
+                       
+                        accidentMessage =  $" :{carName} ran out of gas! ";
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
                         Thread.Sleep(30000);
-                        Console.WriteLine($"<<< {carName} is back on track >>>");
+                        accidentMessage = $" :{carName} is back on track";
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
+                        
+
                     }
 
 
                     if (BirdOnWindshield())
                     {
-                        Console.WriteLine($"!!! A bird landed on {carName}'s windshield! !!!");
+
+
+                        // Clear the previous accident message
+
+                        accidentMessage = $" :A bird landed on {carName}'s windshield!";
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
                         Thread.Sleep(10000);
-                        Console.WriteLine($"<<< {carName} is back on track >>>");
+                        accidentMessage = $" :{carName} is back on track";
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
+
                     }
 
 
                     if (EngineFailure())
                     {
-                        Console.WriteLine($"!!! {carName}'s engine has failed! !!!");
-                        //-1 km / h
                         carSpeed--;
+                        accidentMessage = $" :{carName} engine has failed and now moving at {carSpeed} km/h";
+                        Thread.Sleep(252);
+                        UpdateCarPosition(carName, carIndex, distance, carSpeed, accidentMessage);
                         if (carSpeed <= 0)
                         {
-                            Console.WriteLine($"--- {carName} can't move anymore!----");
+                            Console.Write($"--- {carName} can't move anymore!----");
                             break;
                         }
-                        Console.WriteLine($"*** {carName} is now moving at {carSpeed} km/h ***");
                     }
+
 
                     timer30Seconds = false;
                 }
+                //Distans räknaren "high level matte"
+                distance += carSpeed / (60.0 * 60.0 * 1000.0);
 
-
-
-                //Namnge vinnaren genom att sätta static variabel namn
-                if (i == 10 && winner == "")
+                if (distance >= 10 && winner == "")
                 {
                     //Sätta namn på vinnaren
                     winner = carName;
                 }
-
-                Console.WriteLine();
-                Thread.Sleep(2000);
+                            
             }
 
-            Console.WriteLine($"{carName} finished");
+            DisplayFinishedMessage(carName, carIndex);
         }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         //Alla problem i procentuellt 
@@ -162,6 +176,50 @@
 
 
 
+        //Bilens position i console
+        public static void UpdateCarPosition(string carName, int carIndex, double distance, int carSpeed, string accidentMessage)
+        {
+            lock (consoleLock)
+            {
+                int prevCursorTop = Console.CursorTop;
+                int prevCursorLeft = Console.CursorLeft;
+
+                // Hide the cursor
+                Console.CursorVisible = false;
+
+                Console.SetCursorPosition(0, carPositions[carIndex]);
+
+                // Clear the entire line before displaying the new message
+                Console.Write(new string(' ', Console.WindowWidth - 1));
+                Console.SetCursorPosition(0, carPositions[carIndex]);
+
+                Console.WriteLine($"{carName}: {distance:0.00} km, {carSpeed} km/h {accidentMessage}");
+
+                // Restore the cursor position
+                Console.SetCursorPosition(prevCursorLeft, prevCursorTop);
+
+                // Show the cursor
+                Console.CursorVisible = true;
+            }
+        }
+
+
+
+        //Uppdatera finnish 
+        public static void DisplayFinishedMessage(string carName, int carIndex)
+        {
+            lock (consoleLock)
+            {
+                int prevCursorTop = Console.CursorTop;
+                int prevCursorLeft = Console.CursorLeft;               
+                Console.SetCursorPosition(0, carPositions[carIndex]);              
+                Console.Write(new string(' ', Console.WindowWidth - 1));
+                Console.SetCursorPosition(0, carPositions[carIndex]);
+                // Skriv finished meddelande utan att console.clear
+                Console.WriteLine($"{carName} finished ");
+                Console.SetCursorPosition(prevCursorLeft, prevCursorTop);
+            }
+        }
 
 
 
